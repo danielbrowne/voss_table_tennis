c762fb61637081c4b069103dbd24f63c95769ebb
Daniel Browne <daniel.browne@voss-solutions.com>
2016-12-15 12:43:12 +0200

    Removed unused import

--- ratings/views.py
+++ ratings/views.py
@@ -2,7 +2,7 @@ import datetime
 import json
 
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, Http404
+from django.shortcuts import get_object_or_404
 
 from .models import Person, Game
 
